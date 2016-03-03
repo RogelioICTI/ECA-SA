84c27c3fcd789019cf6a1bb7c1252eb807e86757
Rogelio <rogelio@dsindigo.mx>
2016-03-03 16:58:16 -0600

    Ajustes

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,51 +1,51 @@
 //jQuery to collapse the navbar on scroll
 
-$( document ).ready(function() {
-	
-    if($(".navbar").outerHeight(true)>51){
-		$(".navbar-brand").css('max-width','340px');
-                            $(".imagen_chiquilla").css('margin-top','0px');
-	}else{
-            $(".navbar-brand").css('max-width','280px');
-            $(".imagen_chiquilla").css('margin-top','5px');
-	}
+$(document).ready(function () {
+
+    if ($(".navbar").outerHeight(true) > 51) {
+        $(".navbar-brand").css('max-width', '340px');
+        $(".imagen_chiquilla").css('margin-top', '0px');
+    } else {
+        $(".navbar-brand").css('max-width', '280px');
+        $(".imagen_chiquilla").css('margin-top', '5px');
+    }
 
 });
 
 
-$(window).scroll(function() {
-	
-	
+$(window).scroll(function () {
+
+
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
-            $(".navbar-brand").css('max-width','280px');
-            $(".imagen_chiquilla").css('margin-top','5px');
-		//$("#logo").css('width','30%');
+        $(".navbar-brand").css('max-width', '280px');
+        $(".imagen_chiquilla").css('margin-top', '5px');
+        //$("#logo").css('width','30%');
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");		
-		setTimeout(function(){
-			var elmnt = document.getElementById("navbar_main");
-	
-			if(elmnt.offsetHeight>51){
-                            $(".navbar-brand").css('max-width','340px');
-                            $(".imagen_chiquilla").css('margin-top','0px');
-				//$("#logo").css('width','37%');
-			}
-		},100);
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        setTimeout(function () {
+            var elmnt = document.getElementById("navbar_main");
+
+            if (elmnt.offsetHeight > 51) {
+                $(".navbar-brand").css('max-width', '340px');
+                $(".imagen_chiquilla").css('margin-top', '0px');
+                //$("#logo").css('width','37%');
+            }
+        }, 100);
     }
-		
-	
+
+
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-		
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
+
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
-		
+
         event.preventDefault();
     });
 });
